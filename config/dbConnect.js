e890f7b21a1164cc8bbb7958fb9418e118355d5c
Akhil Reddy <puppireddyakhil@gmail.com>
2023-07-31 12:17:07 +0530

    Final Project

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const dbconnect=async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
         console.log('db connected successfully')
     } catch(error){
         console.log('db connection failed',error.message);
